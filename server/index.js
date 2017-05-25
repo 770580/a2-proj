ee1c2879ff21b82da26db6d1ba9d3444592e18e7
Anton <lascivolin@gmail.com>
2017-05-25 22:14:03 +0300

    add products data mock

--- server/index.js
+++ server/index.js
@@ -1,8 +1,15 @@
 const express = require('express');
+const fs = require('fs');
 const app = express();
 
 app.get('/api/test', function (req, res) {
   res.send('API server works!');
+})
+
+app.get('/api/products', function (req, res) {
+  const fileName = './server/mock.products.json';
+  const data =  fs.readFileSync(fileName, 'utf8');
+  setTimeout(() => (res.send(data)), 1500);
 })
 
 app.listen(3000, function () {
