b2b6f2e42486075a0c268a0ffc141d6b00787a69
Anton <lascivolin@gmail.com>
2017-06-22 00:37:12 +0300

    Products Pagination. Server logic

--- server/index.js
+++ server/index.js
@@ -6,13 +6,13 @@ app.get('/api/test', function (req, res) {
 });
 
 app.get('/api/products', function (req, res) {
-  console.log(req.query);
   const lang = req.query.lang || 'en';
   let products = require('./mock.products.' + lang + '.json').products;
 
   if (req.query.idList) {
     products = products.filter(item => req.query.idList.indexOf(item.id) > -1);
   }
+
   if (req.query.hasOwnProperty('localize')) {
     products = products.map(item => {
       const { id, name, description} = item;
@@ -21,6 +21,12 @@ app.get('/api/products', function (req, res) {
   }
 
   const total = products.length;
+  const offset = Number(req.query.offset);
+  const count = Number(req.query.count);
+
+  if (offset >= 0 && count > 0) {
+    products = products.slice(offset, offset + count);
+  }
 
   setTimeout(() => res.send({ products, total }), 750);
 });
